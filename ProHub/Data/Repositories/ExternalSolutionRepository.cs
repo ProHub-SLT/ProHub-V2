fcc793c9834173ebb29441b432d6ea6ababe4d78
Pasan Pramuditha <pasanpr58@gmail.com>
2026-01-31 01:38:29 +0530

    External Solution Retired Details page update

--- ProHub/Data/Repositories/ExternalSolutionRepository.cs
+++ ProHub/Data/Repositories/ExternalSolutionRepository.cs
@@ -538,6 +538,148 @@ namespace ProHub.Data
             };
         }
 
+        // Get full details of a single retired solution by ID
+        public ExternalPlatform? GetRetiredSolutionByIdFull(int id)
+        {
+            using var conn = GetConnection();
+            conn.Open();
+            string query = @"
+                SELECT 
+                    ep.ID AS Id,
+                    ep.Platform_Name AS PlatformName,
+                    ep.Platform_Type AS PlatformType,
+                    ep.StartDate,
+                    ep.TargetDate,
+                    ep.UATDate,
+                    ep.VADate,
+                    ep.LaunchedDate,
+                    ep.Status,
+                    ep.StatusDate,
+                    ep.BitBucket,
+                    ep.BIT_bucket_repo AS BITBucketRepo,
+                    ep.Integrated_apps AS IntegratedApps,
+                    ep.DR,
+                    ep.Platform_Owner AS PlatformOwner,
+                    ep.APP_OP_Owner AS APP_Owner,
+                    ep.Platform_OTC AS PlatformOTC,
+                    ep.Platform_MRC AS PlatformMRC,
+                    ep.Contract_Period AS ContractPeriod,
+                    ep.Incentive_Earned AS IncentiveEarned,
+                    ep.Incentive_Share AS IncentiveShare,
+                    ep.BillingDate,
+                    ep.Proposal_Upload AS ProposalUploaded,
+                    ep.SLA,
+                    ep.Software_Value AS SoftwareValue,
+                    ep.SSLCertificateExpDate,
+                    ep.DPO_Handover_Date AS DPOHandoverDate,
+                    ep.DPO_Handover_Comment AS DPOHandoverComment,
+                    ep.PercentageDone,
+                    ep.Developed_Team AS DevelopedTeam,
+                    ep.Sales_AM AS SalesAM,
+                    e1.Emp_ID AS DevelopedById,
+                    e1.Emp_Name AS DevelopedByName,
+                    e1.Emp_Email AS DevelopedByEmail,
+                    e1.Emp_Phone AS DevelopedByPhone,
+                    e2.Emp_ID AS BackupOfficer1Id,
+                    e2.Emp_Name AS BackupOfficer1Name,
+                    e2.Emp_Email AS BackupOfficer1Email,
+                    e3.Emp_ID AS BackupOfficer2Id,
+                    e3.Emp_Name AS BackupOfficer2Name,
+                    e3.Emp_Email AS BackupOfficer2Email,
+                    sp.ID AS SDLCStageId,
+                    sp.Phase AS SDLCPhaseName,
+                    c.ID AS CompanyId,
+                    c.Company_Name AS CompanyName,
+                    st.ID AS SalesTeamId,
+                    st.Sales_Team_Name AS SalesTeamName
+                FROM external_platforms ep
+                LEFT JOIN Employee e1 ON ep.Developed_By = e1.Emp_ID
+                LEFT JOIN Employee e2 ON ep.BackupOfficer_1 = e2.Emp_ID
+                LEFT JOIN Employee e3 ON ep.BackupOfficer_2 = e3.Emp_ID
+                LEFT JOIN Company c ON ep.Company_ID = c.ID
+                LEFT JOIN SDLCPhas sp ON ep.SDLCStage = sp.ID
+                LEFT JOIN Sales_Team st ON ep.Sales_Team_ID = st.ID
+                WHERE ep.ID = @id AND (LOWER(TRIM(sp.Phase)) = 'retired' OR LOWER(TRIM(ep.Status)) = 'retired')";
+            using var cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            using var reader = cmd.ExecuteReader();
+            if (!reader.Read()) return null;
+            
+            var platform = new ExternalPlatform
+            {
+                Id = GetValueOrDefault(reader, "Id", 0),
+                PlatformName = GetValueOrDefault(reader, "PlatformName", ""),
+                PlatformType = GetValueOrDefault(reader, "PlatformType", ""),
+                StartDate = reader.IsDBNull(reader.GetOrdinal("StartDate")) ? (DateTime?)null : reader.GetDateTime("StartDate"),
+                TargetDate = reader.IsDBNull(reader.GetOrdinal("TargetDate")) ? (DateTime?)null : reader.GetDateTime("TargetDate"),
+                UATDate = reader.IsDBNull(reader.GetOrdinal("UATDate")) ? (DateTime?)null : reader.GetDateTime("UATDate"),
+                VADate = reader.IsDBNull(reader.GetOrdinal("VADate")) ? (DateTime?)null : reader.GetDateTime("VADate"),
+                LaunchedDate = reader.IsDBNull(reader.GetOrdinal("LaunchedDate")) ? (DateTime?)null : reader.GetDateTime("LaunchedDate"),
+                Status = GetValueOrDefault(reader, "Status", ""),
+                StatusDate = reader.IsDBNull(reader.GetOrdinal("StatusDate")) ? (DateTime?)null : reader.GetDateTime("StatusDate"),
+                BitBucket = GetValueOrDefault(reader, "BitBucket", ""),
+                BITBucketRepo = GetValueOrDefault(reader, "BITBucketRepo", ""),
+                IntegratedApps = GetValueOrDefault(reader, "IntegratedApps", ""),
+                DR = GetValueOrDefault(reader, "DR", ""),
+                PlatformOwner = GetValueOrDefault(reader, "PlatformOwner", ""),
+                APP_Owner = GetValueOrDefault(reader, "APP_Owner", ""),
+                PlatformOTC = reader.IsDBNull(reader.GetOrdinal("PlatformOTC")) ? (decimal?)null : reader.GetDecimal("PlatformOTC"),
+                PlatformMRC = reader.IsDBNull(reader.GetOrdinal("PlatformMRC")) ? (decimal?)null : reader.GetDecimal("PlatformMRC"),
+                ContractPeriod = GetValueOrDefault(reader, "ContractPeriod", ""),
+                IncentiveEarned = reader.IsDBNull(reader.GetOrdinal("IncentiveEarned")) ? (decimal?)null : reader.GetDecimal("IncentiveEarned"),
+                IncentiveShare = reader.IsDBNull(reader.GetOrdinal("IncentiveShare")) ? (decimal?)null : reader.GetDecimal("IncentiveShare"),
+                BillingDate = reader.IsDBNull(reader.GetOrdinal("BillingDate")) ? (DateTime?)null : reader.GetDateTime("BillingDate"),
+                ProposalUploaded = GetValueOrDefault(reader, "ProposalUploaded", ""),
+                SLA = GetValueOrDefault(reader, "SLA", ""),
+                SoftwareValue = reader.IsDBNull(reader.GetOrdinal("SoftwareValue")) ? (decimal?)null : reader.GetDecimal("SoftwareValue"),
+                SSLCertificateExpDate = reader.IsDBNull(reader.GetOrdinal("SSLCertificateExpDate")) ? (DateTime?)null : reader.GetDateTime("SSLCertificateExpDate"),
+                DPOHandoverDate = reader.IsDBNull(reader.GetOrdinal("DPOHandoverDate")) ? (DateTime?)null : reader.GetDateTime("DPOHandoverDate"),
+                DPOHandoverComment = GetValueOrDefault(reader, "DPOHandoverComment", ""),
+                PercentageDone = GetValueOrDefault(reader, "PercentageDone", (decimal?)null),
+                DevelopedTeam = GetValueOrDefault(reader, "DevelopedTeam", ""),
+                SalesAM = GetValueOrDefault(reader, "SalesAM", ""),
+                Company = new Company { Id = GetValueOrDefault(reader, "CompanyId", 0), CompanyName = GetValueOrDefault(reader, "CompanyName", "") },
+                SalesTeam = new SalesTeam { Id = GetValueOrDefault(reader, "SalesTeamId", 0), SalesTeamName = GetValueOrDefault(reader, "SalesTeamName", "") },
+                SDLCStage = new SDLCPhase { Id = GetValueOrDefault(reader, "SDLCStageId", 0), Phase = GetValueOrDefault(reader, "SDLCPhaseName", "Retired") }
+            };
+
+            // Set DevelopedBy employee
+            if (!reader.IsDBNull(reader.GetOrdinal("DevelopedById")))
+            {
+                platform.DevelopedBy = new Employee
+                {
+                    EmpId = GetValueOrDefault(reader, "DevelopedById", 0),
+                    EmpName = GetValueOrDefault(reader, "DevelopedByName", ""),
+                    EmpEmail = GetValueOrDefault(reader, "DevelopedByEmail", ""),
+                    EmpPhone = GetValueOrDefault(reader, "DevelopedByPhone", "")
+                };
+            }
+
+            // Set Backup Officer 1
+            if (!reader.IsDBNull(reader.GetOrdinal("BackupOfficer1Id")))
+            {
+                platform.BackupOfficer1 = new Employee
+                {
+                    EmpId = GetValueOrDefault(reader, "BackupOfficer1Id", 0),
+                    EmpName = GetValueOrDefault(reader, "BackupOfficer1Name", ""),
+                    EmpEmail = GetValueOrDefault(reader, "BackupOfficer1Email", "")
+                };
+            }
+
+            // Set Backup Officer 2
+            if (!reader.IsDBNull(reader.GetOrdinal("BackupOfficer2Id")))
+            {
+                platform.BackupOfficer2 = new Employee
+                {
+                    EmpId = GetValueOrDefault(reader, "BackupOfficer2Id", 0),
+                    EmpName = GetValueOrDefault(reader, "BackupOfficer2Name", ""),
+                    EmpEmail = GetValueOrDefault(reader, "BackupOfficer2Email", "")
+                };
+            }
+
+            return platform;
+        }
+
         // Get full details of all retired solutions (export/report)
         public List<ExternalPlatform> GetRetiredSolutionsFull()
         {

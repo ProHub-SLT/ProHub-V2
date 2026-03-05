a6560366377ac909f814be84122c0be4908f798a
Tharuka Dilshan <148794499+tharuka-tech@users.noreply.github.com>
2026-03-05 09:56:22 +0530

    Project Activitis validate

--- ProHub/Models/ProjectActivity.cs
+++ ProHub/Models/ProjectActivity.cs
@@ -16,6 +16,7 @@ namespace PROHUB.Models
         [Display(Name = "Solution")]
         public int? SolutionId { get; set; }
 
+        [StringLength(2000, ErrorMessage = "Description cannot exceed 2000 characters")]
         [DataType(DataType.MultilineText)]
         public string? Description { get; set; }
 
@@ -28,14 +29,17 @@ namespace PROHUB.Models
         [Display(Name = "Assigned To")]
         public int? AssignedTo { get; set; }
 
+        [Required(ErrorMessage = "Target Date is required")]
         [DataType(DataType.Date)]
         [Display(Name = "Target Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? TargetDate { get; set; }
 
+        [Required(ErrorMessage = "Status is required")]
         public string? Status { get; set; }
 
         [Display(Name = "Updated By")]
+        [RequiredOnEdit(ErrorMessage = "Updated By is required when editing")]
         public int? UpdatedBy { get; set; }
 
         [Display(Name = "Updated Date")]
@@ -67,6 +71,25 @@ namespace PROHUB.Models
 
         [NotMapped]
         public string? LatestComment { get; set; }
+    }
 
+    // Custom Validation Attribute
+    public class RequiredOnEditAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var activity = (ProjectActivity)validationContext.ObjectInstance;
+
+            // If Id > 0, we are Editing. Therefore, validate that UpdatedBy is provided.
+            if (activity.Id > 0)
+            {
+                if (value == null)
+                {
+                    return new ValidationResult(ErrorMessage ?? "This field is required on edit.");
+                }
+            }
+            
+            return ValidationResult.Success;
+        }
     }
 }
